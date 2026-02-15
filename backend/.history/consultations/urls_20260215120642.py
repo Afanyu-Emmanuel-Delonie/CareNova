8fabc05a06c005e3b19a5d23813e2c184ed81058
afanyu <afanyuemma2002@gmail.com>
2026-02-15 12:14:25 +0200

    added an endpoint for medical prescribtions

---
+++ backend/.history/consultations/urls_20260215120642.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CreateDiagnosisView, PatientMedicalHistoryView
+
+urlpatterns = [
+    path('diagnose/', CreateDiagnosisView.as_view(), name='create-diagnosis'),
+    
+    path('history/', PatientMedicalHistoryView.as_view(), name='medical-history'),
+]
---
+++ backend/.history/labs/admin_20260215114232.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import LabResult
+
+@admin.register(LabResult)
+class LabResultsAdmin(admin.ModelAdmin):
+    list_display = ('id')
